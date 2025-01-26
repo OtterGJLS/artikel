d25eea4522bc10ef996aa2a136500942a972a9d4
OtterGJLS <ariffjul43@gmail.com>
2025-01-26 23:12:59 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,5 @@
 
 @section('container')
     <h1>Halaman Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, labore cupiditate dolor eligendi, et obcaecati, quae laudantium reprehenderit saepe quis nisi doloremque aspernatur repudiandae quia. Atque aperiam non provident quam!</p>
 @endsection
