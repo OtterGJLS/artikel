8054a0ce418cde05f92c192b9cc06b1307e53434
OtterGJLS <ariffjul43@gmail.com>
2025-01-27 00:07:21 +0700

    fix: update image source on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
 @section('container')
     <h1>Halaman Home</h1>
-    <a href="https://imgbb.com/"><img class="" src="https://i.ibb.co.com/bKbFs6Y/download.jpg" style="width: 100vw; height: 100vh; object-fit: cover; display: block; margin: 0; padding: 0;"
+    <a href="https://imgbb.com/"><img class="" src="https://i.ibb.co.com/s6vmNLw/ww.jpg" style="width: 100vw; height: 100vh; object-fit: cover; display: block; margin: 0; padding: 0;"
         alt="download" border="0"></a>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, labore cupiditate dolor eligendi, et obcaecati, quae laudantium reprehenderit saepe quis nisi doloremque aspernatur repudiandae quia. Atque aperiam non provident quam!</p>
 @endsection
